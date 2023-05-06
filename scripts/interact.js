cd6ced3884a931b9b7fad84ac29dc587d0b55b27
dbettayeb <dhiaelhak.bettayeb@esprit.tn>
2023-05-06 03:49:56 +0100

    change to root mode before

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,5 +1,6 @@
 const { ethers } = require("ethers");
 const fs = require('fs');
+const { execSync } = require('child_process');
 
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -21,20 +22,21 @@ await rentablenftcontract.mint();
 const balance = await rentablenftcontract.balanceOf("0x6ef69c99a333fef7DdBb6E37efF0b0081BC3fBBE");
 console.log(balance);
 
-await rentablenftcontract.setUser(1,"0xcb21123cD81A08B754735712e099a5Ed5e7748e3"
-,1683338546,macaddress,{ gasLimit });  
+await rentablenftcontract.setUser(4,"0xcb21123cD81A08B754735712e099a5Ed5e7748e3"
+,1683341283,macaddress,{ gasLimit });
+  
 // Check if file exists
-if (fs.existsSync('/home/dbettaieb/Documents/dhia')) {
+if (fs.existsSync('/etc/hostapd/hostapd.whitelist')) {
     // Append message to file
-    fs.appendFileSync('/home/dbettaieb/Documents/dhia', `\n${macaddress}`);
+    fs.appendFileSync('/etc/hostapd/hostapd.whitelist', `\n${macaddress}`);
   } else {
     // Write message to file
-    fs.writeFileSync('/home/dbettaieb/Documents/dhia', `${macaddress}`);
+    fs.writeFileSync('/etc/hostapd/hostapd.whitelist', `${macaddress}`);
   }
-user = await rentablenftcontract.userOf(1);
+user = await rentablenftcontract.userOf(4);
 console.log(user);
 
-userexpire= await rentablenftcontract.userExpires(1);
+userexpire= await rentablenftcontract.userExpires(4);
 console.log(userexpire);
 
 
